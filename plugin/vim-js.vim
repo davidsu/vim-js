c897d0e5048a213074fd5b65c1a864ec75d9cd47
David <david.susskind@walkme.com>
2020-12-28 00:01:40 +0200

    start porting mySqlMru to javascript
        current changes support fixing the broken autocd on vimEnter for
        monorepo which behaved strangely.
        go together with commit f0ee5a7 of dotfiles

---
+++ plugin/vim-js.vim
@@ -0,0 +1,2 @@
+autocmd VimEnter * call MySqlMruVimEnter()
+
--- rplugin/node/vim-js/index.js
+++ rplugin/node/vim-js/index.js
@@ -1,7 +1,10 @@
 const goToDeclaration = require('./src/goToDeclaration')
+const mySqlMru = require('./src/mySqlMru')
 const { init } = require('./src/api')
 module.exports = plugin => {
-  plugin.setOptions({ dev: true, alwaysInit: true })
+  // plugin.setOptions({ dev: true, alwaysInit: true })
+  plugin.setOptions({ dev: false })
   init(plugin)
   goToDeclaration(plugin)
+  mySqlMru(plugin)
 }
